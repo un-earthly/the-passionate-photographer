8d2e1f542cef261d1646b8d9614165f7edbd6d83
un-earthly <vijayalamin@gmail.com>
2022-04-16 21:35:14 -0700

    gallery added

--- src/Pages/Home/Gallery.js
+++ src/Pages/Home/Gallery.js
@@ -3,36 +3,51 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import useImgs from '../../Hooks/useImgs';
 
+
 export default function Gallery() {
     const { imgs } = useImgs()
     const catNature = imgs.filter(img => img.category === 'nature')
+    const catWedding = imgs.filter(img => img.category === 'wedding')
+    const catFashion = imgs.filter(img => img.category === 'fashion')
+    const catBeach = imgs.filter(img => img.category === 'beach')
 
+    const showBigImg = (img) => {
+
+    }
     return (
         <Tabs focusTabOnClick={false} className=' mt-14 text-center'>
             <TabList>
-                <Tab>All</Tab>
                 <Tab>Nature</Tab>
+                <Tab>Wedding</Tab>
                 <Tab>Fashion And Beauty</Tab>
+                <Tab>Beach</Tab>
             </TabList>
 
             <TabPanel>
-                <div className="grid grid-cols-3 gap-4">
+                <div className="grid grid-cols-3 gap-4 w-3/4 mx-auto">
                     {
-                        catNature.map(nature => <img src={nature.picture} alt="" />)
+                        catNature.map(img => <img className='duration-500 filter h-full object-cover hover:brightness-50 cursor-pointer' onClick={() => showBigImg(img.picture)} src={img.picture} alt="" />)
                     }
                 </div>
             </TabPanel>
             <TabPanel>
-                <div className="grid grid-cols-3 gap-4">
+                <div className="grid grid-cols-3 gap-4 w-3/4 mx-auto">
                     {
-                        imgs.length
+                        catWedding.map(img => <img className='duration-500 filter h-full object-cover hover:brightness-50 cursor-pointer' onClick={() => showBigImg(img.picture)} src={img.picture} alt="" />)
                     }
                 </div>
             </TabPanel>
             <TabPanel>
-                <div className="grid grid-cols-3 gap-4">
+                <div className="grid grid-cols-3 gap-4 w-3/4 mx-auto">
                     {
-                        imgs.length
+                        catFashion.map(img => <img className='duration-500 filter h-full object-cover hover:brightness-50 cursor-pointer' onClick={() => showBigImg(img.picture)} src={img.picture} alt="" />)
+                    }
+                </div>
+            </TabPanel>
+            <TabPanel>
+                <div className="grid grid-cols-3 gap-4 w-3/4 mx-auto">
+                    {
+                        catBeach.map(img => <img className='duration-500 filter h-full object-cover hover:brightness-50 cursor-pointer' onClick={() => showBigImg(img.picture)} src={img.picture} alt="" />)
                     }
                 </div>
             </TabPanel>
